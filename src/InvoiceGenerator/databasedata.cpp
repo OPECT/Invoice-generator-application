5d468e34a19855edf63707d0c139a9d7bc2e5ec9
OPECT <oserafyn@gmail.com>
2015-07-03 09:22:37 +0300

    Fixed bug with wrong attributes for new database

--- src/InvoiceGenerator/databasedata.cpp
+++ src/InvoiceGenerator/databasedata.cpp
@@ -1,7 +1,7 @@
 #include "databasedata.h"
 
-DataBaseData::DataBaseData() : m_columndId("id"), m_goodsNameColumn("Name"), m_goodsCategoryColumn("Category"),
-     m_goodsPriceColumn("Price"), m_customerNameColumn("Name"), m_customerRegionColumn("Region"),
+DataBaseData::DataBaseData() : m_columndId("id"), m_goodsNameColumn("Name"), m_goodsTypeColumn("Type"),
+    m_goodsCategoryColumn("Category"), m_goodsPriceColumn("Price"), m_customerNameColumn("Name"), m_customerRegionColumn("Region"),
      m_customerMultiplierColumn("Multiplier")
 {
 }
